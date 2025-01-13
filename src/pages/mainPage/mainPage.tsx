5e2ca9540928a77636f81a2e1a6df876ce316435
Akshay <singh12345akshay@gmail.com>
2025-01-14 00:40:51 +0530

    initial project setup

---
+++ src/pages/mainPage/mainPage.tsx
@@ -0,0 +1,13 @@
+
+
+/**
+ * @function Main Page
+ * @returns {JSX.Element}
+ */
+export default function MainPage(): JSX.Element {
+  return (
+    <div className="bg-slate-50 h-screen w-full">
+     This is main page
+    </div>
+  );
+}
